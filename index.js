4492775bdfbd3ede8a659073442b7e3c7eec5fad
Ben Robuck <brobuck87@gmail.com>
2022-11-26 08:35:46 -0500

    Update index.js to complete menu system. Start of template literal to output HTML file

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ const fs = require("fs");
 const path = require("path");
 const inquirer = require("inquirer");
 const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const generateSite = require("./src/generateSite");
 // const generateSite = require("./src/generateSite");
 
 const managerQuestions = [
@@ -26,27 +29,109 @@ const managerQuestions = [
     message: "What is the manager's office number?",
   },
 ];
+
+const otherRoleQuestions = [
+  {
+    type: "list",
+    name: "menu",
+    message: "Do you want to add another role, or exit?",
+    choices: ["Engineer", "Intern", "Exit"],
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of the engineer?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the ID of the engineer?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the email of the engineer?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "What is the github of the engineer?",
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "What is the name of the intern?",
+  },
+  {
+    type: "input",
+    name: "id",
+    message: "What is the ID of the intern?",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "What is the email of the intern?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "What school does the intern go to?",
+  },
+];
 // WHEN I start the application
 function writeToFile(fileName, data) {
   return fs.writeFileSync(path.join(process.cwd(), fileName), data);
 }
 
-function init() {
-  inquirer
-    .prompt(managerQuestions)
-    .then(({ name, id, email, officeNumber }) => {
-      const managerResponses = new Manager(name, id, email, officeNumber);
-      console.log(managerResponses);
-      // writeToFile("index.html", generateSite({ ...inquirerResponses }));
-    });
+async function init() {
+  const employees = [];
+  const { name, id, email, officeNumber } = await inquirer.prompt(
+    managerQuestions
+  );
+  const managerResponses = new Manager(name, id, email, officeNumber);
+  employees.push(managerResponses);
+  const roster = await mainMenu(employees);
+  const site = generateSite(roster);
+  writeToFile("index.html", site);
+}
+
+async function mainMenu(employees) {
+  const roleOrExit = await inquirer.prompt(otherRoleQuestions);
+  if (roleOrExit.menu == "Engineer") {
+    const newEngineer = await addEngineer();
+    employees.push(newEngineer);
+    mainMenu(employees);
+  } else if (roleOrExit.menu == "Intern") {
+    const newIntern = await addIntern();
+    employees.push(newIntern);
+    mainMenu(employees);
+  } else {
+    console.log(employees);
+    return employees;
+  }
+}
+
+async function addEngineer() {
+  // prompt user for engineer name, ID, email and github
+  const { name, id, email, github } = await inquirer.prompt(engineerQuestions);
+  // create new engineer object
+  const engineerResponses = new Engineer(name, id, email, github);
+  // return engineer
+  return engineerResponses;
+}
+
+async function addIntern() {
+  // prompt user for Intern name, ID, email and school
+  const { name, id, email, school } = await inquirer.prompt(internQuestions);
+  // create new engineer object
+  const internResponses = new Intern(name, id, email, school);
+  // return engineer
+  return internResponses;
 }
 init();
-// THEN I am prompted to enter the team manager’s name, employee ID, email address, and office number
-// WHEN I enter the team manager’s name, employee ID, email address, and office number
-// THEN I am presented with a menu with the option to add an engineer or an intern or to finish building my team
-// WHEN I select the engineer option
-// THEN I am prompted to enter the engineer’s name, ID, email, and GitHub username, and I am taken back to the menu
-// WHEN I select the intern option
-// THEN I am prompted to enter the intern’s name, ID, email, and school, and I am taken back to the menu
-// WHEN I decide to finish building my team
-// THEN I exit the application, and the HTML is generated
--- src/generateSite.js
+++ src/generateSite.js
@@ -1,1 +1,30 @@
+// Create a function to generate site
+function generateSite(data) {
+  return `<!DOCTYPE html>
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Employee Roster</title>
+      <!-- //Tailwind// -->
+      <script src="https://cdn.tailwindcss.com"></script>
+      <!-- //Flowbite// -->
+      <link
+        rel="stylesheet"
+        href="https://unpkg.com/flowbite@1.5.2/dist/flowbite.min.css"
+      />
+    </head>
+  
+    <body>
+
+<a href="#" class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow-md hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
+<p class="font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
+</a>
+
+      <script src="https://unpkg.com/flowbite@1.5.2/dist/flowbite.js"></script>
+    </body>
+  </html>`;
+}
 module.exports = generateSite;
